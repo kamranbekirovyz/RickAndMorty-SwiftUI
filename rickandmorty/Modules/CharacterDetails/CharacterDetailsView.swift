000fce47e6877a8ca88bdd9e74d94f8bf3827b81
PORELARTE <kamranbekirovyz@gmail.com>
2022-11-27 23:41:56 +0400

    Implemented CharacterDetailsView

--- rickandmorty/Modules/CharacterDetails/CharacterDetailsView.swift
+++ rickandmorty/Modules/CharacterDetails/CharacterDetailsView.swift
@@ -24,13 +24,50 @@ struct CharacterDetailsView: View {
   
   var body: some View {
     NavigationView {
-      VStack {
-        AsyncImage(url: URL(string: item.image))
-          .clipShape(Circle())
+      List() {
+        VStack(){
+          VStack(alignment: .leading) {
+            AsyncImage(url: URL(string: item.image)) { image in
+              image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+          } placeholder: {
+            ProgressView()
+          }
+              .frame(width: 64, height: 64)
+              .scaledToFit()
+              .clipShape(Circle())
+              .padding(24)
+            
+            buildProperty(label: "Species", description: item.species)
+            buildProperty(label: "Gender", description: item.gender)
+            buildProperty(label: "Status", description: item.status)
+            buildProperty(label: "Type", description: item.type)
+            buildProperty(label: "Episodes", description: item.episode.first ?? "unknown")
+            buildProperty(label: "Location", description: item.location.name)
+            buildProperty(label: "Origin", description: item.origin.name)
+            buildProperty(label: "Created", description: item.created)
+            
+          }
+          
+        }
       }
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .navigationBarTitle(item.name)
+      .listStyle(.insetGrouped)
     }
+  }
+  
+  private func buildProperty(
+    label: String,
+    description: String
+  ) -> some View {
+    HStack() {
+      
+      Text(label).font(.headline)
+      Spacer(minLength: .ulpOfOne)
+      Text(description).font(.subheadline)
+    }
+    .padding(2)
+  
   }
 }
 
